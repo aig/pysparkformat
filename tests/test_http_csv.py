036c2ef7a7c8b170b5a88a9322ccf7e12bdff3c5
aig <389247+aig@users.noreply.github.com>
2024-11-22 21:07:32 +0100

    add http-csv options

--- tests/test_http_csv.py
+++ tests/test_http_csv.py
@@ -9,33 +9,55 @@ from pysparkformat.http.csv import HTTPCSVDataSource
 
 
 class TestHttpCsv(unittest.TestCase):
-    def test_http_csv(self):
+    @classmethod
+    def setUpClass(cls):
         os.environ["PYSPARK_PYTHON"] = sys.executable
 
         if sys.platform == "win32":
             hadoop_home = Path(__file__).parent.parent / "tools" / "windows" / "hadoop"
             os.environ["HADOOP_HOME"] = str(hadoop_home)
-            os.environ["PATH"] = os.environ["PATH"] + ";" + str(hadoop_home / "bin")
+            os.environ["PATH"] += ";" + str(hadoop_home / "bin")
 
-        spark = SparkSession.builder.appName("custom-datasource-example").getOrCreate()
+        cls.spark = SparkSession.builder.appName("http-csv-test-app").getOrCreate()
+        cls.spark.dataSource.register(HTTPCSVDataSource)
 
-        spark.dataSource.register(HTTPCSVDataSource)
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
 
+    def test_valid_csv_with_header(self):
         url = (
             "https://raw.githubusercontent.com/aig/pysparkformat/"
             + "refs/heads/master/tests/data/valid-csv-with-header.csv"
         )
 
         result = (
-            spark.read.format("http-csv")
+            self.spark.read.format("http-csv")
             .option("header", True)
-            .option("maxLineSize", 10000)
             .load(url)
             .localCheckpoint()
         )
 
-        assert result.count() == 50985
+        self.assertEqual(result.count(), 50985)
 
+    def test_valid_csv_without_header(self):
+        url = (
+            "https://raw.githubusercontent.com/aig/pysparkformat/"
+            + "refs/heads/master/tests/data/valid-csv-with-header.csv"
+        )
+
+        result = (
+            self.spark.read.format("http-csv")
+            .option("header", False)
+            .load(url)
+            .localCheckpoint()
+        )
+
+        self.assertEqual(result.count(), 50986)
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 if __name__ == "__main__":
     unittest.main()
