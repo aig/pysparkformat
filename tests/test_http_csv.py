a46f7256748a100e994c6cf1c463595e22a1da33
aig <389247+aig@users.noreply.github.com>
2024-11-22 21:26:51 +0100

    add http-csv options add additional test data

--- tests/test_http_csv.py
+++ tests/test_http_csv.py
@@ -9,6 +9,15 @@ from pysparkformat.http.csv import HTTPCSVDataSource
 
 
 class TestHttpCsv(unittest.TestCase):
+    VALID_CSV_WITH_HEADER = (
+        "https://raw.githubusercontent.com/aig/pysparkformat/"
+        + "refs/heads/master/tests/data/valid-csv-with-header.csv"
+    )
+    VALID_CSV_WITHOUT_HEADER = (
+        "https://raw.githubusercontent.com/aig/pysparkformat/"
+        + "refs/heads/master/tests/data/valid-without-header.csv"
+    )
+
     @classmethod
     def setUpClass(cls):
         os.environ["PYSPARK_PYTHON"] = sys.executable
@@ -26,34 +35,14 @@ class TestHttpCsv(unittest.TestCase):
         cls.spark.stop()
 
     def test_valid_csv_with_header(self):
-        url = (
-            "https://raw.githubusercontent.com/aig/pysparkformat/"
-            + "refs/heads/master/tests/data/valid-csv-with-header.csv"
-        )
-
         result = (
             self.spark.read.format("http-csv")
             .option("header", True)
-            .load(url)
+            .load(self.VALID_CSV_WITH_HEADER)
             .localCheckpoint()
         )
 
         self.assertEqual(result.count(), 50985)
-
-    def test_valid_csv_without_header(self):
-        url = (
-            "https://raw.githubusercontent.com/aig/pysparkformat/"
-            + "refs/heads/master/tests/data/valid-csv-with-header.csv"
-        )
-
-        result = (
-            self.spark.read.format("http-csv")
-            .option("header", False)
-            .load(url)
-            .localCheckpoint()
-        )
-
-        self.assertEqual(result.count(), 50986)
 
 
 if __name__ == "__main__":
